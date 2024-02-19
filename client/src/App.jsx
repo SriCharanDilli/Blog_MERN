8ef094c4473fec53895be4ecf7444b9cf5b941a9
OgCliche <sricharan039@gmail.com>
2024-02-19 21:34:27 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import SignIn from './Pages/SignIn'
+import Projects from './Pages/Projects'
 function App() {
   return (
-    <div className='text-ellipsis text-blue-800'>
-      App
-    </div>
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/about' element={<About/>}/>
+    <Route path='/signin' element={<SignIn/>}/>
+    <Route path='/projects' element={<Projects/>}/>
+
+  </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/Pages/SignUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SignUp() {
+  return (
+    <div>r
+      
+    </div>
+  )
+}
+
+export default SignUp
