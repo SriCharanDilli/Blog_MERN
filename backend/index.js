260891a9351f084d174dab90895edba4d965ad2e
OgCliche <sricharan039@gmail.com>
2024-02-20 20:24:33 +0530

    database Connected

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,19 @@
-import express from 'express'
-const app= express();
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-app.listen(3000,()=>{
-    console.log("app is listening")
-})
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.DATABASE_URI)
+    .then(() => {
+        console.log("Connected to MongoDB");
+        app.listen(port, () => {
+            console.log(`App is listening on port ${port}`);
+        });
+    })
+    .catch((error) => {
+        console.error("Error connecting to MongoDB:", error);
+    });
